6062704359a4e54f2043f63f14bf574491d150ec
황유빈 <bnm0218@naver.com>
2024-12-10 18:20:24 +0900

    테스팅 라이브러리와 접근성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+    render(<App />);
+    const headingElement = screen.getByRole("heading", { name: /learn react/i });
+    expect(headingElement).toBeInTheDocument();
 });
