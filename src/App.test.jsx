92864a03c753b86b92fb1365ba86d59a83bf96d4
황유빈 <bnm0218@naver.com>
2024-12-11 01:56:00 +0900

    버튼 클릭 시 테스트 행동

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,31 @@
-import { logRoles, render, screen } from "@testing-library/react";
+import { fireEvent, logRoles, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("button start with correct color", () => {
-    const { container } = render(<App />);
-    logRoles(container)
+test("button click flow", () => {
+    // render App
+    render(<App />);
+
+    // find the button
     const buttonElement = screen.getByRole("button", { name: /blue/i });
+    
+    // check initial color
     expect(buttonElement).toHaveClass("red");
+
+    //click the button
+    fireEvent.click(buttonElement)
+
+    //check button text
+    buttonElement.toHaveTextContent(/red/i)
+    
+    //check the button color
+    expect(buttonElement).toHaveClass('blue')
+
 });
-test("button has with correct color after click", () => {});
-test("button has with correct text after click", () => {});
+test("button has with correct color after click", () => {
+    
+
+    
+
+    // 
+});
+
